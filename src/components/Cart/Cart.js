fbefff575ceb3a82de5affcb9ef05f467fd5c10e
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-09-23 10:12:41 +0600

    project done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,11 +11,24 @@ const Cart = (props) => {
   for (const product of cart) {
     total = total + product.price;
   }
+
+  let shipping = 0;
+  for (const product of cart) {
+    shipping = shipping + 20;
+  }
+  const tax = (total + shipping) / 5;
+  const grandtotal = total + shipping + tax;
   return (
     <div>
       <h3 className="text-center">Order Summary</h3>
       <h5 className="text-center">Items ordered: {props.cart.length} </h5>
-      <p className="lead text-center fw-bold">Total: {total} </p>
+      <p>Shipping & Handing: ${shipping} </p>
+      <p>Total Tax: ${tax.toFixed(2)} </p>
+      <p className="lead fw-bold text-danger border-bottom">Order Total: ${total.toFixed(2)} </p>
+      <p className="fs-2 lead">Grand Total: ${grandtotal.toFixed(2)} </p>
+      <div className="text-center">
+        <button className="btn btn-warning">Review your order</button>
+      </div>
     </div>
   );
 };
