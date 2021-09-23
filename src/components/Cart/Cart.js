22404d25c49b875bf5a19af2f869561a43cbbfa3
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-09-23 09:44:41 +0600

    simple ema john project

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,9 @@ import "./Cart.css";
 const Cart = (props) => {
   //   console.log(props.cart);
   const { cart } = props;
+
+  /* const totalReducer = (previous, product) => previous + product.price;
+  const total = cart.reduce(totalReducer, 0); */
   let total = 0;
   for (const product of cart) {
     total = total + product.price;
